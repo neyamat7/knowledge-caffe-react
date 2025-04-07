2bf99c38cd5c1e3c776800035e595d25ad54834b
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-04-07 12:59:34 +0600

    changes logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,13 @@ import Profile from "../profile/Profile";
 const navbar = () => {
   return (
     <div className="flex justify-between items-center max-w-screen-xl mx-auto px-5 mt-6 border-b border-slate-400 pb-5">
-      <h1 className="text-4xl font-bold">Knowledge Caffe</h1>
+      <h1 className="text-4xl font-bold">TechTide</h1>
 
       {/* profile pic */}
-      <Profile></Profile>
+      {/* <Profile></Profile> */}
+      <div className="w-16 h-16">
+        <img className="w-full h-full rounded-full" src="/techtide.png" alt="" />
+      </div>
     </div>
   );
 };
